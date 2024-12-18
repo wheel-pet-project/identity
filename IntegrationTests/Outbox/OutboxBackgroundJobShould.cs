9ac31d64ef687a558f74b22b05b0446b17515899
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-12-18 14:03:24 +0300

    add tests for OutboxBackgroundJob

--- IntegrationTests/Outbox/OutboxBackgroundJobShould.cs
+++ IntegrationTests/Outbox/OutboxBackgroundJobShould.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Core.Domain.ConfirmationTokenAggregate;
 using Core.Ports.Postgres;
 using Infrastructure.Adapters.Postgres;
 using Infrastructure.Adapters.Postgres.Outbox;
@@ -7,46 +8,76 @@ using Infrastructure.Settings;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Newtonsoft.Json;
 using Npgsql;
+using Quartz;
+using Testcontainers.PostgreSql;
 using Xunit;
 
 namespace IntegrationTests.Outbox;
 
-public class OutboxBackgroundJobShould
+public class OutboxBackgroundJobShould : IntegrationTestBase
 {
+    [Fact]
+    public async Task CanReadAndMediatorPublishNotificationTwoTimes()
+    {
+        // Arrange
+        var aggregate = ConfirmationToken.Create(accountId: Guid.NewGuid(), new string('*', 60));
+        aggregate.AddCreatedDomainEvent(Guid.NewGuid());
+        aggregate.AddCreatedDomainEvent(Guid.NewGuid());
+        
+        var unitOfWorkAndOutboxBuilder = new UnitOfWorkAndOutboxBuilder();
+        unitOfWorkAndOutboxBuilder.ConfigureConnection(PostgreSqlContainer.GetConnectionString());
+        var (unitOfWork, outbox) = unitOfWorkAndOutboxBuilder.Build();
+        
+        await unitOfWork.BeginTransaction();
+        await outbox.PublishDomainEvents(aggregate);
+        await unitOfWork.Commit();
+        
+        var outboxBackgroundJobBuilder = new OutboxBackgroundJobBuilder();
+        outboxBackgroundJobBuilder.ConfigureConnection(PostgreSqlContainer.GetConnectionString());
+        var outboxBackJob = outboxBackgroundJobBuilder.Build();
+
+        // Act
+        await outboxBackJob.Execute(new Mock<IJobExecutionContext>().Object);
+
+        // Assert
+        Assert.True(outboxBackgroundJobBuilder.VerifyMediatorPublishMethod(2));
+    }
     
     private class OutboxBackgroundJobBuilder
     {
         private DbSession _session = null!;
         private readonly Mock<IMediator> _mediatorMock = new();
         
-        OutboxBackgroundJob Build() => new(_session, _mediatorMock.Object);
+        public OutboxBackgroundJob Build() => new(_session, _mediatorMock.Object);
 
-        public void ConfigureConnection(string connectionString)
-        {
+        public void ConfigureConnection(string connectionString) =>
             _session = new DbSession(new NpgsqlConnection(connectionString));
+
+        public bool VerifyMediatorPublishMethod(int times)
+        {
+            _mediatorMock.Verify(x => x.Publish(It.IsAny<INotification>(), It.IsAny<CancellationToken>()),
+                Times.Exactly(times));
+            return true;
         }
     }
     
     private class UnitOfWorkAndOutboxBuilder
     {
-        private string _connectionString = null!;
-        private IDbConnection _connection = null!;
         private DbSession _session = null!;
         private readonly Mock<ILogger<PostgresRetryPolicy>> _postgresRetryPolicyLoggerMock = new();
 
-        public (DbSession ,IUnitOfWork, IOutbox) Build()
+        public (IUnitOfWork, IOutbox) Build()
         {
             var unitOfWork = new UnitOfWork(_session, new PostgresRetryPolicy(_postgresRetryPolicyLoggerMock.Object));
             var outbox = new Infrastructure.Adapters.Postgres.Outbox.Outbox(_session);
-            return (_session, unitOfWork, outbox);
+            return (unitOfWork, outbox);
         }
 
         public void ConfigureConnection(string connectionString)
         {
-            _connectionString = connectionString;
-            _connection = new NpgsqlConnection(_connectionString);
-            _session = new DbSession(_connection);
+            _session = new DbSession(new NpgsqlConnection(connectionString));
         }
     }
 }
